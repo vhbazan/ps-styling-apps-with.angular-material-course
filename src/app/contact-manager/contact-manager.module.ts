67d10c09ba634f7c68b33ee1b1f8bb127b8559ab
Victor Hugo Bazan <vhbazanm@gmail.com>
2021-10-15 16:40:17 +0200

    Add contact-manager module and comp

---
+++ src/app/contact-manager/contact-manager.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactManagerComponent } from './contact-manager.component';
+
+
+
+@NgModule({
+  declarations: [
+    ContactManagerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ContactManagerModule { }
