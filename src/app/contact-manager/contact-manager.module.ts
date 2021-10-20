837605615adad283bdf4b50db591a67ec1690d0f
Victor Hugo Bazan <vhbazanm@gmail.com>
2021-10-20 10:04:02 +0200

    Scaffold app

--- src/app/contact-manager/contact-manager.module.ts
+++ src/app/contact-manager/contact-manager.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+
 import { ContactManagerComponent } from './contact-manager.component';
+import { ToolBarComponent } from './components/tool-bar/tool-bar.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
 
 
-
+const routes: Routes = [
+  { path: '',  component: ContactManagerComponent, children: [
+    { path: '', component: MainContentComponent }
+  ] },
+  { path: '**', redirectTo: ''}
+];
 @NgModule({
   declarations: [
-    ContactManagerComponent
+    ContactManagerComponent,
+    ToolBarComponent,
+    MainContentComponent,
+    SideNavComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class ContactManagerModule { }
